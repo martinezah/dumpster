2885099600e1f165efc90e7eee37acb9e2ae05a1
Marti Martinez <marti@argenticsoftware.com>
2013-03-07 13:40:55 -0800

    barebones zend application

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+
+    }
+
+    public function preDispatch()
+    {
+
+    }
+
+    public function indexAction()
+    {
+
+    }
+}
