a146613d73d5f376cecd68c4a3c84c0cdc33fb9a
Marti Martinez <marti@argenticsoftware.com>
2013-03-10 23:39:22 -0700

    toggle expanded state

--- public/diver/js/app.js
+++ public/diver/js/app.js
@@ -8,15 +8,6 @@ function DumpsterCtrl($scope, $http) {
         var tags = $('#tags option:selected').map(function(){ return this.text }).get();
         $http.post('/', {action: 'find', tags: tags}).success( function(data) { if (data && data.dumps) $scope.dumps = data.dumps;}); 
     };
-    $scope.expand = function(e, dump) { e.target.innerHTML = "<pre>" + JSON.stringify(dump.data, null, 2) + "</pre>"; };
-
-/*
-    $scope.selectTag = function(tag) { if ($.inArray(tag, $scope.selectedTags) < 0) $scope.selectedTags.push(tag); };
-    $scope.deselectTag = function(tag) { var ii = $.inArray(tag, $scope.selectedTags); if (ii >= 0) $scope.selectedTags.splice(ii, 1); };
-    $scope.get = function() { if ($scope.getId) $http.post('/', {action:'get',id:$scope.getId}).success( function(data) { if (data && data.dump && data.dump.id == $scope.getId) $scope.getData = data.dump; }); }
-    $scope.getPubkey = function() { $http.post('/', {action:'pubkey'}).success( function(data) { if (data && data.pubkey) { $scope.pubkey = data.pubkey; } }); };
-    $scope.togglePubkey = function() { if (!$scope.pubkey) $scope.getPubkey(); $scope.showPubkey = !$scope.showPubkey; }
-*/   
 
     //initialization
     $scope.tags();
